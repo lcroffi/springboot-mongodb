031cb8bea129cb1319d266053fdbf57321e7ab1e
lcroffi <lileekat@hotmail.com>
2018-12-27 17:07:26 -0200

    Consulta simples com @query

--- src/main/java/com/lcroffi/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/lcroffi/springbootmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lcroffi.springbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lcroffi.springbootmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.lcroffi.springbootmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> findByTitleContainingIgnoreCase(String text); // função query method com palavras-chave do Spring com Mongo
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
